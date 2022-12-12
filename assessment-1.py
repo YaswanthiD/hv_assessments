d47a316db0aa4a25495a2e1b0f12c3d01284ee3d
Yaswanthi D <88917427+YaswanthiD@users.noreply.github.com>
2022-12-12 14:48:22 +0530

    Add files via upload

---
+++ assessment-1.py
@@ -0,0 +1,10 @@
+a1=int(input())
+a2=int(input())
+a3=int(input())
+a4=int(input())
+a5=int(input())
+# b=a1=a2=a3=a4=a5
+if(a1<=0 or a2<=0 or a3<=0 or a4<=0 or a5<=0):
+    print("Enter all positive number")
+else:
+    print("Sum of given positive numbers is: ",a1+a2+a3+a4+a5)
